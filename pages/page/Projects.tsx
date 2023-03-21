3a20e194d2c87c42a8a6ca587f4585b44a714e93
jaywest <jaywest650@gmail.com>
2023-03-21 14:48:41 +0000

    link

--- pages/page/Projects.tsx
+++ pages/page/Projects.tsx
@@ -4,40 +4,42 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Project from "../../src/components/Project";
 import { projectInfo } from "./../../src/data";
 const Projects = () => {
-    const data = projectInfo;
+     const data = projectInfo;
 
-    gsap.registerPlugin(ScrollTrigger);
+     gsap.registerPlugin(ScrollTrigger);
 
-    useEffect(() => {
-        const el = document.querySelectorAll(".list section");
-        el.forEach((e) => {
-            gsap.from(e, {
-                scrollTrigger: {
-                    trigger: e,
-                    start: "top center",
-                },
-                duration: 2,
-                opacity: 0,
-                y: 100,
-            });
-        });
-    }, []);
-    return (
-        <div id="projects" className="h-full  space-y-14 p-5 pt-20">
-            <div className="flex items-center">
-                <h1 className="text-2xl font-semibold ">
-                    <small className="text-md text-[#64FFDA] mr-4">03.</small>
-                    Some Things I Have Built
-                </h1>
-                <div className="h-[1px] bg-[#233554] w-[200px] ml-5" />
-            </div>
-            <section className="space-y-28 flex flex-col list">
-                {data.map((value, i) => (
-                    <Project data={value} key={i} index={i} />
-                ))}
-            </section>
-        </div>
-    );
+     useEffect(() => {
+          const el = document.querySelectorAll(".list section");
+          el.forEach((e) => {
+               gsap.from(e, {
+                    scrollTrigger: {
+                         trigger: e,
+                         start: "top center",
+                    },
+                    duration: 2,
+                    opacity: 0,
+                    y: 100,
+               });
+          });
+     }, []);
+     return (
+          <div id="projects" className="h-full  space-y-14 p-5 pt-20">
+               <div className="flex items-center">
+                    <h1 className="text-2xl font-semibold ">
+                         <small className="text-md text-[#64FFDA] mr-4">
+                              03.
+                         </small>
+                         Some Things I Have Built
+                    </h1>
+                    <div className="h-[1px] bg-[#233554] w-[200px] ml-5" />
+               </div>
+               <section className="space-y-28 flex flex-col list">
+                    {data.map((value, i) => (
+                         <Project data={value} key={i} index={i} />
+                    ))}
+               </section>
+          </div>
+     );
 };
 
 export default Projects;
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,56 +1,60 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { FiGithub } from "react-icons/fi";
 import { IoMdOpen } from "react-icons/io";
 
 const Project = ({ data, index }: any) => {
-    const reverse = index % 2 !== 0;
-    return (
-        <section
-            className={`md:flex  space-x-5 items-center capitalize cursor-pointer ${
-                reverse ? "md:flex-row-reverse" : ""
-            } `}
-        >
-            <div className=" w-[87%] md:w-[600px] h-[350px] absolute  md:relative group rounded-sm overflow-hidden mx-5 -z-50 md:z-50 ">
-                <Image src={data.img} alt="" layout="fill" />
-                <div className="absolute w-full h-full bg-[#0B1B31]  md:bg-[#0a192f9a] md:group-hover:hidden" />
-            </div>
-            <div
-                className={` flex-1  space-y-4 ${
-                    reverse ? "" : "md:text-end"
-                } `}
-            >
-                <h1 className={`text-[#64FFDA]  `}>Featured Project</h1>
-                <h2 className="font-bold text-lg text-[#CCD6F6]">
-                    {data.name}
-                </h2>
-                <div className="  bg-[#112240] w-full p-6 px-3 text-[#A8B2D1]">
-                    {data.description}
-                </div>
-                <div
-                    className={`flex items-center space-x-5 ${
-                        reverse ? "md:justify-start" : "md:justify-end"
-                    }  `}
-                >
-                    {data.tools.map((value: any, i: any) => (
-                        <p className="text-[#A0ABCA]">{value}</p>
-                    ))}
-                </div>
-                <div
-                    className={`flex ${
-                        reverse ? "" : "md:justify-end"
-                    } space-x-5 text-xl`}
-                >
-                    <a href={data.git} target="_blank">
-                        <FiGithub />
-                    </a>
-                    <a href={data.link} target="_blank">
-                        <IoMdOpen />
-                    </a>
-                </div>
-            </div>
-        </section>
-    );
+     const reverse = index % 2 !== 0;
+     return (
+          <section
+               className={`md:flex  space-x-5 items-center capitalize cursor-pointer ${
+                    reverse ? "md:flex-row-reverse" : ""
+               } `}
+          >
+               <Link href={data.link} target="_blank">
+                    <div className=" w-[87%] md:w-[600px] h-[350px] absolute  md:relative group rounded-sm overflow-hidden mx-5 -z-50 md:z-50 ">
+                         <Image src={data.img} alt="" layout="fill" />
+                         <div className="absolute w-full h-full bg-[#0B1B31]  md:bg-[#0a192f9a] md:group-hover:hidden" />
+                    </div>
+               </Link>
+
+               <div
+                    className={` flex-1  space-y-4 ${
+                         reverse ? "" : "md:text-end"
+                    } `}
+               >
+                    <h1 className={`text-[#64FFDA]  `}>Featured Project</h1>
+                    <h2 className="font-bold text-lg text-[#CCD6F6]">
+                         {data.name}
+                    </h2>
+                    <div className="  bg-[#112240] w-full p-6 px-3 text-[#A8B2D1]">
+                         {data.description}
+                    </div>
+                    <div
+                         className={`flex items-center space-x-5 ${
+                              reverse ? "md:justify-start" : "md:justify-end"
+                         }  `}
+                    >
+                         {data.tools.map((value: any, i: any) => (
+                              <p className="text-[#A0ABCA]">{value}</p>
+                         ))}
+                    </div>
+                    <div
+                         className={`flex ${
+                              reverse ? "" : "md:justify-end"
+                         } space-x-5 text-xl`}
+                    >
+                         <a href={data.git} target="_blank">
+                              <FiGithub />
+                         </a>
+                         <a href={data.link} target="_blank">
+                              <IoMdOpen />
+                         </a>
+                    </div>
+               </div>
+          </section>
+     );
 };
 
 export default Project;
